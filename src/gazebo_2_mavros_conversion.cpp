f80fc398befbe9254202fe81b48fc77277ab6cd6
Grace Pearcey <pearcey.grace@gmail.com>
2024-01-14 19:05:47 -0500

    remove unessesary includes

--- src/gazebo_2_mavros_conversion.cpp
+++ src/gazebo_2_mavros_conversion.cpp
@@ -1,22 +1,6 @@
-#include <mavros_msgs/CommandTOL.h>
-#include <mavros_msgs/State.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <cmath>
-#include <math.h>
 #include <ros/ros.h>
-#include <std_msgs/Float64.h>
-#include <std_msgs/String.h>
-#include <mavros_msgs/CommandBool.h>
-#include <mavros_msgs/SetMode.h>
-#include <mavros_msgs/PositionTarget.h>
-#include <unistd.h>
-#include <vector>
-#include <ros/duration.h>
-#include <iostream>
-
-#include <task_master/TaskGoalPosition.h>
 
 ros::Subscriber desired_pos_gazebo_sub;
 ros::Publisher desired_pos_mavros_pub;
--- src/mavros_2_gazebo_conversion.cpp
+++ src/mavros_2_gazebo_conversion.cpp
@@ -1,22 +1,22 @@
-#include <mavros_msgs/CommandTOL.h>
-#include <mavros_msgs/State.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/Pose.h>
+//#include <mavros_msgs/CommandTOL.h>
+//#include <mavros_msgs/State.h>
+//#include <nav_msgs/Odometry.h>
+//#include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <cmath>
-#include <math.h>
+//#include <math.h>
 #include <ros/ros.h>
-#include <std_msgs/Float64.h>
-#include <std_msgs/String.h>
-#include <mavros_msgs/CommandBool.h>
-#include <mavros_msgs/SetMode.h>
-#include <mavros_msgs/PositionTarget.h>
-#include <unistd.h>
-#include <vector>
-#include <ros/duration.h>
-#include <iostream>
-
-#include <task_master/TaskGoalPosition.h>
+//#include <std_msgs/Float64.h>
+//#include <std_msgs/String.h>
+//#include <mavros_msgs/CommandBool.h>
+//#include <mavros_msgs/SetMode.h>
+//#include <mavros_msgs/PositionTarget.h>
+//#include <unistd.h>
+//#include <vector>
+//#include <ros/duration.h>
+//#include <iostream>
+//
+//#include <task_master/TaskGoalPosition.h>
 
 
 
